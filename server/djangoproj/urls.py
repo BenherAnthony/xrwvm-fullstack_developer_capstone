f92aca9b313b65dd8247ac8ce510ac8a69c97dd7
BenherAnthony <benheranthony2000@gmail.com>
2026-04-22 16:43:05 -0400

    Added About and Contact pages, updated settings and URLs

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -5,9 +5,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Home route (fixes 404)
+    # Home
     path('', TemplateView.as_view(template_name="About.html")),
 
-    # About page
+    # About
     path('about/', TemplateView.as_view(template_name="About.html")),
+
+    # Contact (NEW)
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ]
