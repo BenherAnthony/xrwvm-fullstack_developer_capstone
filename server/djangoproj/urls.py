d7c53f2f19e4a2d1664bcd039fc7656ebc14d0fc
BenherAnthony <benheranthony2000@gmail.com>
2026-04-22 17:48:13 -0400

    Added Register component

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Home
-    path('', TemplateView.as_view(template_name="About.html")),
+    # Backend APIs
+    path('djangoapp/', include('djangoapp.urls')),
 
-    # About
+    # React routes
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+
+    # Static pages
     path('about/', TemplateView.as_view(template_name="About.html")),
-
-    # Contact (NEW)
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ]
